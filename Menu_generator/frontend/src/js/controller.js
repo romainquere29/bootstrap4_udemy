40f9c0b09dfd15a61553a2d0a159e65826e8cdfd
Romain QUERE <rquere@tribun.health>
2023-06-30 20:06:55 +0200

    imporve menu display

--- Menu_generator/frontend/src/js/controller.js
+++ Menu_generator/frontend/src/js/controller.js
@@ -1,27 +1,39 @@
 
 
 const form = document.querySelector('.form');
-const startDate = document.getElementById('startDate');
-const endDate = document.getElementById('endDate');
+const startForm = document.getElementById('startDate');
+const endForm = document.getElementById('endDate');
+const isLaunch = document.getElementById('launch');
+const isDinner = document.getElementById('diner');
 
+ 
 class Menu {
     // date = new Date();
     // id = (Date.now() + '').slice(-10);
     // clicks = 0;
+    state = {
+        MenuTable : [],
+    }
+    
 
-
-    constructor(startDate, endDate, people, dejeuner, diner, entree, plats, desserts, auto) {
-        this.startDate = startDate; 
-        this.endDate = endDate; 
-        this.people = people; 
-        this.dejeuner = dejeuner; 
-        this.diner = diner; 
-        this.entree = entree; 
-        this.plats = plats; 
-        this.desserts = desserts; 
-        this.auto = auto; 
+    constructor() {
+        this._init();
+        // this.entree = entree; 
+        // this.plats = plats; 
+        // this.desserts = desserts; 
+        // this.auto = auto; 
 
         this._SubmitEvent();
+    }
+
+    _init() {
+        this.state.MenuTable= [];
+
+        this.startForm = startForm;
+        this.endForm = endForm; 
+        // this.people = people; 
+        this.isLaunch = isLaunch; 
+        this.isDinner = isDinner; 
     }
 
     _SubmitEvent() {
@@ -31,9 +43,32 @@ class Menu {
         // })
     }
 
-    _newMenu() {
-        console.log(`date de debut : ${startDate.value}`);
-        console.log(`date de fin : ${endDate.value}`);
+    _calcMenuTable() {
+        // console.log(this.state.MenuTable);
+        let id=0;
+        let tableDate = this.state.startDate;
+        while (tableDate <= this.state.endDate) {
+            if (this.isLaunch?.checked)
+                this.state.MenuTable.push({date : tableDate.toDateString(), repas: this.isLaunch.value, id : id});
+                id+=1;
+            if (this.isDinner?.checked)
+                console.log('here1');
+                this.state.MenuTable.push({date : tableDate.toDateString(), repas: this.isDinner.value, id : id});
+                id+=1;
+                tableDate.setDate(tableDate.getDate() + 1);
+        }
+        console.log(this.state);
+
+    }
+
+    _newMenu(event) {
+        event.preventDefault();
+        this._init();
+        this.state.startDate = new Date(this.startForm.value.split('/')[2], this.startForm.value.split('/')[1], this.startForm.value.split('/')[0]); 
+        this.state.endDate = new Date(this.endForm.value.split('/')[2], this.endForm.value.split('/')[1], this.endForm.value.split('/')[0]); 
+        // this${(this.endDate - this.startDate) / (1000*3600*24)}
+        // console.log(`${(this.endDate - this.startDate) / (1000*3600*24)}`)
+        this._calcMenuTable();
     }
 
 }
